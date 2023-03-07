a4f9a7d86b173f52946a51229d4bc916a612b156
saidatla201 <saidatla201@gmail.com>
2023-03-07 12:58:22 +0530

    Adding Pop and Peek

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace StackandQueue
             public static void Main(string[] args)
             {
                 Console.WriteLine("Welcome to Stack and Queue using Linked List");
-            Console.WriteLine("Welcome to Stack and Queue using Linked List");
+
+
+            
             Stack stack = new Stack();
             stack.Push(70);
             stack.Push(30);
