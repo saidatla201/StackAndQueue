c02382a80a243e08f9fc7b10abe9ff48939908ea
saidatla201 <saidatla201@gmail.com>
2023-03-07 12:42:01 +0530

    Adding Stack to the Program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace StackandQueue
+{
+   class Program
+    {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Stack and Queue using Linked List");
+                Stack stack = new Stack();
+                stack.Push(70);
+                stack.Push(30);
+                stack.Push(56);
+                stack.Display();
+                Console.ReadLine();
+            }
+        }
+    }
+
---
+++ Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackandQueue
+{
+    class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node newnode = new Node(value);
+            if (this.top == null)
+            {
+                newnode.next = null;
+            }
+            else
+            {
+                newnode.next = this.top;
+            }
+            this.top = newnode;
+            Console.WriteLine("{0} Pushed to the stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("Stack data : " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
+
