0f4ba37e4235ad80718dd2acfde55e8757470423
saidatla201 <saidatla201@gmail.com>
2023-03-07 13:36:28 +0530

    Remove the data by Dequeue

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace StackandQueue
     {
             public static void Main(string[] args)
             {
-           
+
             Console.WriteLine("Welcome to Stack and Queue using Linked List");
 
             Stack stack = new Stack();
@@ -21,12 +21,11 @@ namespace StackandQueue
             Console.WriteLine("\nQueue");
 
             Queue queue = new Queue();
-            queue.Enqueue(70);
-            queue.Enqueue(30);
             queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Dequeue();
             queue.Display();
-            
-
             Console.ReadLine();
         }
         }
--- Queue.cs
+++ Queue.cs
@@ -6,45 +6,61 @@ using System.Threading.Tasks;
 
 namespace StackandQueue
 {
-   
-        class Queue
+
+    class Queue
+    {
+        Node Front;
+        Node Rear;
+        public Queue()
         {
-            Node Front;
-            Node Rear;
-            public Queue()
+            this.Front = null;
+            this.Rear = null;
+        }
+        public void Enqueue(int value)
+        {
+            Node newnode = new Node(value);
+            if (this.Rear == null)
             {
-                this.Front = null;
+                this.Front = this.Rear;
+                this.Rear = newnode;
+            }
+            else
+            {
+                this.Rear.next = newnode;
+                this.Rear = newnode;
+            }
+            Console.WriteLine("{0} node added to Queue", value);
+        }
+        public void Dequeue()
+        {
+            if (this.Front == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            Node temp = this.Front;
+            this.Front = this.Front.next;
+            if (this.Front == null)
+            {
                 this.Rear = null;
             }
-            public void Enqueue(int value)
+            Console.WriteLine("{0} node removed from Queue", temp.data);
+        }
+        public void Display()
+        {
+            Node temp = this.Rear;
+            if (temp != null)
             {
-                Node newnode = new Node(value);
-                if (this.Rear == null)
-                {
-                    this.Front = this.Rear;
-                    this.Rear = newnode;
-                }
-                else
-                {
-                    this.Rear.next = newnode;
-                    this.Rear = newnode;
-                }
-                Console.WriteLine("{0} node added to Queue", value);
+                Console.WriteLine("Queue is Empty");
+                return;
             }
-            public void Display()
+            this.Rear = this.Rear.next;
+            while (this.Rear != null)
             {
-                Node temp = this.Rear;
-                if (temp == null)
-                {
-                    Console.WriteLine("Queue is Empty");
-                    return;
-                }
-                while (temp != null)
-                {
-                    Console.WriteLine("Queue Data : " + temp.data);
-                    temp = temp.next;
-                }
+                Console.WriteLine("Queue Data : " + temp.data);
+                temp = temp.next;
             }
+        }
         }
     }
 
